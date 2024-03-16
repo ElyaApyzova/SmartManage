4d5348168ff7a7cf41b1db35dd4ad06b8533a56e
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-03-16 23:37:41 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import Header from './components/Header';
+import Header from "./components/Header/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
    <div>
-    <BrowserRouter>
+    <Router>
    <Header />
    <Routes>
     <Route path="/" element={<Home />} />
    </Routes>
-   </BrowserRouter>
+   <Footer />
+   </Router>
    </div>
     
   );
