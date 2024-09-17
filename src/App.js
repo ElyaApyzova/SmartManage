655125a724749f624dc6db083e05c62a95970db3
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-17 13:37:30 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-import Header from "./components/Header/Header";
-import Home from "./components/Home";
-import Footer from "./components/Footer/Footer";
+
 
 
 function App() {
-  return (
-   <div>
-    <Router>
-   <Header />
-   <Routes>
-    <Route path="/" element={<Home />} />
-   </Routes>
-   <Footer />
-   </Router>
-   </div>
-    
-  );
+  
 }
 
 export default App;
